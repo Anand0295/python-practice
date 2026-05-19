cc2d29129041d7f96006024e13d88afa38264dcc
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-19 17:35:09 +0530

    auto-sync: CRUD change

---
+++ if-else.py
@@ -0,0 +1,366 @@
+# if else condition:
+
+# if condition:
+
+# |----------|
+
+# |   TSB    |
+
+# |----------|
+
+# else:
+
+# |----------|
+
+# |   FSB    |
+
+# |----------|
+
+# 1.wap to check whether a number is even or odd.
+
+# n = int(input('enter a number: '))
+
+# if n % 2 == 0:
+
+# print(f'{n} is even')
+
+# else:
+
+# print(f'{n} is odd')
+
+"""
+enter a number: 10
+10 is even
+"""
+
+# 2.wap to check whether a person is eligible for driving.
+
+# age = int(input('enter age: '))
+
+# if age >= 18:
+
+# print('eligible for driving')
+
+# else:
+
+# print('not eligible for driving')
+
+"""
+enter age: 15
+not eligible for driving
+"""
+
+# 3.wap to find the greater number between two numbers.
+
+# a = int(input('enter first number: '))
+
+# b = int(input('enter second number: '))
+
+# if a > b:
+
+# print(f'{a} is greater')
+
+# else:
+
+# print(f'{b} is greater')
+
+"""
+enter first number: 10
+enter second number: 20
+20 is greater
+"""
+
+# 4.wap to check whether a character is vowel or consonant.
+
+# ch = input('enter a character: ')
+
+# if ch in 'aeiouAEIOU':
+
+# print(f'{ch} is vowel')
+
+# else:
+
+# print(f'{ch} is consonant')
+
+"""
+enter a character: b
+b is consonant
+"""
+
+# 5.wap to check whether a student is pass or fail.
+
+# marks = int(input('enter marks: '))
+
+# if marks >= 35:
+
+# print('pass')
+
+# else:
+
+# print('fail')
+
+"""
+enter marks: 20
+fail
+"""
+
+# 6.wap to check whether a number is positive or negative.
+
+# n = int(input('enter a number: '))
+
+# if n > 0:
+
+# print(f'{n} is positive')
+
+# else:
+
+# print(f'{n} is negative')
+
+"""
+enter a number: -2
+-2 is negative
+"""
+
+# 7.wap to check whether a number is divisible by 2 or not.
+
+# n = int(input('enter a number: '))
+
+# if n % 2 == 0:
+
+# print(f'{n} is divisible by 2')
+
+# else:
+
+# print(f'{n} is not divisible by 2')
+
+"""
+enter a number: 7
+7 is not divisible by 2
+"""
+
+# 8.wap to check whether a number is divisible by both 5 and 11.
+
+# n = int(input('enter a number: '))
+
+# if n % 5 == 0 and n % 11 == 0:
+
+# print(f'{n} is divisible by both 5 and 11')
+
+# else:
+
+# print(f'{n} is not divisible by both 5 and 11')
+
+"""
+enter a number: 20
+20 is not divisible by both 5 and 11
+"""
+
+# 9.wap to check whether a year is leap year or not.
+
+# year = int(input('enter a year: '))
+
+# if year % 4 == 0:
+
+# print(f'{year} is leap year')
+
+# else:
+
+# print(f'{year} is not leap year')
+
+"""
+enter a year: 2023
+2023 is not leap year
+"""
+
+# 10.wap to check whether a character is uppercase or lowercase.
+
+# ch = input('enter a character: ')
+
+# if ch.isupper():
+
+# print(f'{ch} is uppercase')
+
+# else:
+
+# print(f'{ch} is lowercase')
+
+"""
+enter a character: a
+a is lowercase
+"""
+
+# 11.wap to find the smallest among two numbers.
+
+# a = int(input('enter first number: '))
+
+# b = int(input('enter second number: '))
+
+# if a < b:
+
+# print(f'{a} is smaller')
+
+# else:
+
+# print(f'{b} is smaller')
+
+"""
+enter first number: 40
+enter second number: 10
+10 is smaller
+"""
+
+# 12.wap to check whether a person is senior citizen or not.
+
+# age = int(input('enter age: '))
+
+# if age >= 60:
+
+# print('senior citizen')
+
+# else:
+
+# print('not senior citizen')
+
+"""
+enter age: 45
+not senior citizen
+"""
+
+# 13.wap to check whether temperature is hot or cold.
+
+# temp = int(input('enter temperature: '))
+
+# if temp > 30:
+
+# print('hot')
+
+# else:
+
+# print('cold')
+
+"""
+enter temperature: 20
+cold
+"""
+
+# 14.wap to check whether a number is multiple of 3 or not.
+
+# n = int(input('enter a number: '))
+
+# if n % 3 == 0:
+
+# print(f'{n} is multiple of 3')
+
+# else:
+
+# print(f'{n} is not multiple of 3')
+
+"""
+enter a number: 10
+10 is not multiple of 3
+"""
+
+# 15.wap to check whether a password is correct or wrong.
+
+# password = input('enter password: ')
+
+# if password == 'python123':
+
+# print('correct password')
+
+# else:
+
+# print('wrong password')
+
+"""
+enter password: hello
+wrong password
+"""
+
+# 16.wap to check whether a number is zero or non-zero.
+
+# n = int(input('enter a number: '))
+
+# if n == 0:
+
+# print('zero')
+
+# else:
+
+# print('non-zero')
+
+"""
+enter a number: 5
+non-zero
+"""
+
+# 17.wap to check whether a person is child or adult.
+
+# age = int(input('enter age: '))
+
+# if age < 18:
+
+# print('child')
+
+# else:
+
+# print('adult')
+
+"""
+enter age: 22
+adult
+"""
+
+# 18.wap to check whether entered fruit is mango or not.
+
+# fruit = input('enter fruit name: ')
+
+# if fruit == 'mango':
+
+# print('it is mango')
+
+# else:
+
+# print('it is not mango')
+
+"""
+enter fruit name: apple
+it is not mango
+"""
+
+# 19.wap to check whether a number is greater than 500 or not.
+
+# n = int(input('enter a number: '))
+
+# if n > 500:
+
+# print(f'{n} is greater than 500')
+
+# else:
+
+# print(f'{n} is not greater than 500')
+
+"""
+enter a number: 300
+300 is not greater than 500
+"""
+
+# 20.wap to compare two strings and check whether they are equal or not.
+
+# a = input('enter first string: ')
+
+# b = input('enter second string: ')
+
+# if a == b:
+
+# print('both strings are equal')
+
+# else:
+
+# print('both strings are not equal')
+
+"""
+enter first string: hi
+enter second string: hello
+both strings are not equal
+"""
