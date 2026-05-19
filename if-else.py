f3fb4ded08a84b69964e0dc2030adb6593168da9
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-19 17:47:14 +0530

    auto-sync: CRUD change

--- if-else.py
+++ if-else.py
@@ -127,11 +127,11 @@ enter a number: 20
 """
 
 # 9.wap to check whether a year is leap year or not.
-year = int(input("enter a year: "))
-if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-    print(f"{year} is leap year")
-else:
-    print(f"{year} is not leap year")
+# year = int(input("enter a year: "))
+# if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+#     print(f"{year} is leap year")
+# else:
+#     print(f"{year} is not leap year")
 
 """
 enter a year: 2024
