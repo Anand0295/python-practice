1030930070dafad3eb06c32161fea49d30945847
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-20 17:55:43 +0530

    auto-sync: CRUD change

--- while.py
+++ while.py
@@ -126,12 +126,13 @@ enter a number: 15
 n = int(input("enter a number: "))
 rev = 0
 while n > 0:
-    rev = rev * 10 + n % 10
+    rev = rev * 10 + n % 10  #
     n = n // 10
 print(rev)
 
 """
-
+enter a number: 256
+652
 """
 # TODO: 9. WAP to check whether given number is palindrome or not using while loop.
 # TODO: 10. WAP to traverse through a string using while loop.
