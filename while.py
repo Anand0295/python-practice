09bf0ea3e6145845f728220469e07a43d02f7f9f
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-20 22:11:26 +0530

    auto-sync: CRUD change

--- while.py
+++ while.py
@@ -541,7 +541,11 @@ while i < len(lst1):
     if len(lst1[i]) % 2 == 0:
         lst2.append((lst1[i], i))
     else:
-        lst2.append(())
+        lst2.append((lst1[i], len(lst1[i])))
+    i += 1
+print(lst2)
+
+
 # TODO: 24. WAP to create a list with word and reverse word pair inside a tuple using while loop.
 # TODO: 25. WAP to create a dictionary with key as word and value as index of the word using while loop.
 
