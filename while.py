62ce9da13519efe585a125cde49c6c7a1cf5de2e
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-20 21:59:35 +0530

    auto-sync: CRUD change

--- while.py
+++ while.py
@@ -491,7 +491,7 @@ while i < len(lst1):
     if len(lst1[i]) % 2 == 0:
         lst2.append(i)
     else:
-        lst2.append(lst1[i])
+        lst2.append(lst1[i][::-1])
 
 
 # TODO: 22. WAP to traverse through a list and check whether the word starts with vowel or consonant, if vowel add as it is else reverse the word and add to a new list using while loop.
