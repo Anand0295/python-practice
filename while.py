ab5022785cb17fbb363bfbc75f9c73c85a0e011b
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-20 22:06:02 +0530

    auto-sync: CRUD change

--- while.py
+++ while.py
@@ -513,6 +513,8 @@ lst1 = ["apple", "even", "odd", "hi", "spider"]
 i = 0
 lst2 = []
 while i < len(lst1):
+    # word[0] --> first letter in "aeiouAEIOU" --> vowel checking
+
     if lst1[i][0] == "aeiouAEIOU":
         lst2.append(lst1[i])
     else:
