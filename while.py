133ec32e6505aa4a7a9ed182beda779b98c344c1
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-20 09:12:59 +0530

    auto-sync: CRUD change

--- while.py
+++ while.py
@@ -153,10 +153,10 @@ enter a number: 15
 # TODO: 31. WAP to count vowels and consonants in a string using while loop.
 # TODO: 32. WAP to print numbers from 1 to 50 divisible by 5 using while loop.
 # TODO: 33. WAP to print numbers from 50 to 1 using while loop.
-n = 50
-while n > 0:
-    print(n, end=" ")
-    n -= 1
+# n = 50
+# while n > 0:
+#     print(n, end=" ")
+#     n -= 1
 
 """
 50 49 48 47 46 45 44 43 42 41 40 39 38 37 36 35 34 33 32 31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
