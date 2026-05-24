521bee779b886151a94559cee43121a36df277b0
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-24 08:01:27 +0000

    PM9571:31 PM144-27PMve: ${24PM1:31 PMe} ${1:31 PMi31e}

--- for.py
+++ for.py
@@ -391,9 +391,9 @@ enter a number: 10
 """
 
 # 30. WAP to print multiplication table
-n = int(input("enter a number: "))
-for i in range(1, n + 1):
-    print(f"{n} * {i} = {i * n}")
+# n = int(input("enter a number: "))
+# for i in range(1, n + 1):
+#     print(f"{n} * {i} = {i * n}")
 
 """
 enter a number: 15
@@ -415,6 +415,10 @@ enter a number: 15
 """
 
 # 31. WAP to print multiplication tables from 1 to 10
+for i in range(1, 11):
+    print("Table of", i)
+    for j in range(1, 11):
+        print(f"{n} * {i} = {i}")
 # 32. WAP to print squares from 1 to n
 # 33. WAP to print cubes from 1 to n
 # 34. WAP to print factorial of number
