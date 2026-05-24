77d5eb4e2bbb95c0ddd0efe790cf36a4b81e4d74
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-24 08:12:09 +0000

    PM0991:42 PM144-9PMve: ${24PM1:42 PMe} ${1:42 PMi42e}

--- for.py
+++ for.py
@@ -666,7 +666,38 @@ K L M N O
 # 40. WAP to print ASCII values A-Z
 for i in range(65, 91):
     print(chr(i), "=", i)
+
+"""
+A = 65
+B = 66
+C = 67
+D = 68
+E = 69
+F = 70
+G = 71
+H = 72
+I = 73
+J = 74
+K = 75
+L = 76
+M = 77
+N = 78
+O = 79
+P = 80
+Q = 81
+R = 82
+S = 83
+T = 84
+U = 85
+V = 86
+W = 87
+X = 88
+Y = 89
+Z = 90
+"""
+
 # 41. WAP to print ASCII values a-z
+
 # 42. WAP to print divisors of number
 # 43. WAP to check prime number
 # 44. WAP to print prime numbers from 1 to n
