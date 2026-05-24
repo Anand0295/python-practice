9fd3480cd29172218bff46a2f49023d06b31b06b
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-24 07:49:09 +0000

    PM4481:19 PM144-9PMve: ${24PM1:19 PMe} ${1:19 PMi19e}

--- for.py
+++ for.py
@@ -363,8 +363,7 @@ enter a string: hello
 """
 
 # 27. WAP to print numbers from 10 to 1
-n = 1
-for i in range(10, n):
+for i in range(10, 0, -1):
     print(i, end=" ")
 # 28. WAP to print even numbers from 1 to 100
 # 29. WAP to print odd numbers from 1 to n
