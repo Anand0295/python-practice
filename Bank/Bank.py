515ba8b0250698acf744db7900ff8de0ea9dae12
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-24 08:59:49 +0000

    PM9912:29 PM144-49PMve: ${24PM2:29 PMe} ${2:29 PMi29e}

--- Bank/Bank.py
+++ Bank/Bank.py
@@ -4,3 +4,19 @@ database = {"user1": ["user@123", 1234, 1000], "user2": ["python#123", 5678, 0]}
 def validate_password(password):
     upper = 0
     lower = 0
+    special = 0
+    digit = 0
+
+    for i in password:
+        if i.isupper():
+            upper += 1
+        elif i.islower():
+            lower += 1
+        elif i.isspecial():
+            special += 1
+        elif i.isdigit():
+            digit += 1
+
+    if len(password) >= 8 and upper >= 1 and lower >= 1 and digit >= 1 and special >= 1:
+        return True
+    return False
