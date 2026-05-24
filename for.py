0b8e76964d439fc300c091123b2cc74cae47149b
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-24 07:51:16 +0000

    PM5301:21 PM144-16PMve: ${24PM1:21 PMe} ${1:21 PMi21e}

--- for.py
+++ for.py
@@ -374,7 +374,16 @@ enter a string: hello
 for i in range(1, 101):
     if i % 2 == 0:
         print(i, end=" ")
+
+"""
+2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64 66 68 70 72 74 76 78 80 82 84 86 88 90 92 94 96 98 100
+"""
+
 # 29. WAP to print odd numbers from 1 to n
+n = int(input("enter a number: "))
+for i in range(1, n + 1):
+    if i % 2 != 0:
+        print(i)
 # 30. WAP to print multiplication table
 # 31. WAP to print multiplication tables from 1 to 10
 # 32. WAP to print squares from 1 to n
