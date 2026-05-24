950bcb5f2e7e9f355354ea304cb657fe55009f58
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-24 08:05:49 +0000

    PM9781:35 PM144-49PMve: ${24PM1:35 PMe} ${1:35 PMi35e}

--- for.py
+++ for.py
@@ -579,7 +579,7 @@ n = int(input("enter a number: "))
 fact = 1
 for i in range(1, n + 1):
     fact = fact * i
-print
+print("")
 # 35. WAP to print Fibonacci series
 # 36. WAP to print star pattern
 # 37. WAP to print reverse star pattern
