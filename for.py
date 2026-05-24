8308e23175e335eeaa2e9d1c12eef32c7a628a23
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-24 07:58:33 +0000

    PM2331:28 PM144-33PMve: ${24PM1:28 PMe} ${1:28 PMi28e}

--- for.py
+++ for.py
@@ -391,6 +391,9 @@ enter a number: 10
 """
 
 # 30. WAP to print multiplication table
+n = int(input("enter a number: "))
+for i in range(1, n + 1):
+    print("")
 # 31. WAP to print multiplication tables from 1 to 10
 # 32. WAP to print squares from 1 to n
 # 33. WAP to print cubes from 1 to n
