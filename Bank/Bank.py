638fff85fd8f85b18274f2090046250a8a4d83b0
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-24 10:01:55 +0000

    PM8833:31 PM144-55PMve: ${24PM3:31 PMe} ${3:31 PMi31e}

--- Bank/Bank.py
+++ Bank/Bank.py
@@ -129,3 +129,243 @@ elif op == "2":
     signup()
 else:
     print("Invalid option")
+
+"""
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 2
+
+enter your new username: user1
+Sorry, Username already Exists!!
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 2
+
+enter your new username: user3
+enter your new password: weak
+Enter a valid password.
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 2
+
+enter your new username: user3
+enter your new password: Strong@123
+Confirm you Password: Strong@999
+passwords do not match!!
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 2
+
+enter your new username: user3
+enter your new password: Strong@123
+Confirm you Password: Strong@123
+enter your new pin: 12
+Enter a valid pin.
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 2
+
+enter your new username: user3
+enter your new password: Strong@123
+Confirm you Password: Strong@123
+enter your new pin: 1234
+Confirm your pin: 9999
+pins do not match!!
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 2
+
+enter your new username: user3
+enter your new password: Strong@123
+Confirm you Password: Strong@123
+enter your new pin: 1234
+Confirm your pin: 1234
+enter the opening amount: 100
+minimum deposit is Rs.500
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 2
+
+enter your new username: user3
+enter your new password: Strong@123
+Confirm you Password: Strong@123
+enter your new pin: 1234
+Confirm your pin: 1234
+enter the opening amount: 1000
+Account created successfully!!
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 1
+
+Enter your username: user99
+Username does not Exist!!
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 1
+
+Enter your username: user1
+Enter your password: wrongpass
+Wrong Password!!
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 1
+
+Enter your username: user1
+Enter your password: User@123
+1. Deposit
+2. Withdraw
+3. Balance Check
+4. Exit
+Choose your option: 1
+
+           DEPOSIT           
+
+enter your pin: 9999
+Pin not matching!!
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 1
+
+Enter your username: user1
+Enter your password: User@123
+1. Deposit
+2. Withdraw
+3. Balance Check
+4. Exit
+Choose your option: 1
+
+           DEPOSIT           
+
+enter your pin: 1234
+Enter amount you are depositing: 500
+Amount deposited successfully!!
+New Balance: Rs.1500
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 1
+
+Enter your username: user1
+Enter your password: User@123
+1. Deposit
+2. Withdraw
+3. Balance Check
+4. Exit
+Choose your option: 2
+
+           WITHDRAW            
+
+enter your pin: 1234
+Enter amount you are withdrawing: 9999
+Insufficient Balance!!
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 1
+
+Enter your username: user1
+Enter your password: User@123
+1. Deposit
+2. Withdraw
+3. Balance Check
+4. Exit
+Choose your option: 2
+
+           WITHDRAW            
+
+enter your pin: 1234
+Enter amount you are withdrawing: 500
+Amount withdrawn successfully!!
+New Balance: Rs.500
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 1
+
+Enter your username: user1
+Enter your password: User@123
+1. Deposit
+2. Withdraw
+3. Balance Check
+4. Exit
+Choose your option: 3
+
+           BALANCE           
+
+enter your pin: 1234
+Current Balance: Rs.1000
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 1
+
+Enter your username: user1
+Enter your password: User@123
+1. Deposit
+2. Withdraw
+3. Balance Check
+4. Exit
+Choose your option: 4
+Good Bye!
+
+---
+
+Welcome to the Bank
+1. Login
+2. Signup
+Choose: 9
+Invalid option
+"""
