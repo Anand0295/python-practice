7c0fee1c62b09c61cbbdf30a211d65233fd0aa02
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-24 07:51:33 +0000

    PM0101:21 PM144-33PMve: ${24PM1:21 PMe} ${1:21 PMi21e}

--- for.py
+++ for.py
@@ -371,9 +371,9 @@ enter a string: hello
 """
 
 # 28. WAP to print even numbers from 1 to 100
-for i in range(1, 101):
-    if i % 2 == 0:
-        print(i, end=" ")
+# for i in range(1, 101):
+#     if i % 2 == 0:
+#         print(i, end=" ")
 
 """
 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64 66 68 70 72 74 76 78 80 82 84 86 88 90 92 94 96 98 100
