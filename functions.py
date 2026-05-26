cd04be2dadd61b35da7290c7cd9733abffa2cb75
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-26 13:15:19 +0000

    PM4356:45 PM146-19PMve: ${26PM6:45 PMe} ${6:45 PMi45e}

--- functions.py
+++ functions.py
@@ -28,6 +28,11 @@
 
 # QUESTIONS:
 # 1. WAP to print hello world using function
+def prnt():
+    print("hello world")
+
+
+print()
 # 2. WAP to print your name using function
 # 3. WAP to print numbers from 1 to 10 using function
 # 4. WAP to print even numbers using function
