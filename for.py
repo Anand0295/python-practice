fab312a3eaecbd026cec5cf19136bc41a460ac09
Anand <153408885+Anand0295@users.noreply.github.com>
2026-05-27 05:43:20 +0000

    AM29511:13 AM147-20AMve: ${27AM11:13 AMe} ${11:13 AMi13e}

--- for.py
+++ for.py
@@ -1444,7 +1444,9 @@ n x different
 """
 
 # 90. WAP to replace missing values with '*'
-s1 = "pyt"
+s1 = "pyth"
+s2 = "on"
+
 # 91. WAP to print longest paired sequence
 # 92. WAP to compare unequal tuples
 # 93. WAP to align names and marks
